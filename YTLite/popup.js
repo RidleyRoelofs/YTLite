b36ecb2a0db19b885d63713bb14c72b7057a98f3
fredc1 <frederick.cunningham98@gmail.com>
2023-01-21 16:12:47 -0500

    now all the sliders work

--- YTLite/popup.js
+++ YTLite/popup.js
@@ -1,9 +1,21 @@
 // Get a reference to the toggle button
-const toggleButton = document.getElementById('toggle-button');
+const toggleButtonShorts = document.getElementById('toggle-button-shorts');
+const toggleButtonRecs = document.getElementById('toggle-button-recs');
+const toggleButtonAll = document.getElementById('toggle-button-all');
 
-// Add a click event listener to the button
-toggleButton.addEventListener('click', () => {
+
+toggleButtonShorts.addEventListener('click', () => {
   // Toggle the "on" class on the toggle button
-  toggleButton.classList.toggle('on');
+  toggleButtonShorts.classList.toggle('on');
+});
+
+toggleButtonRecs.addEventListener('click', () => {
+  // Toggle the "on" class on the toggle button
+  toggleButtonRecs.classList.toggle('on');
+});
+
+toggleButtonAll.addEventListener('click', () => {
+  // Toggle the "on" class on the toggle button
+  toggleButtonAll.classList.toggle('on');
 });
 
